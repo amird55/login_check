af4d5f03d6a721ceef1f4081b197dbd5a9f8f90b
amird55 <amir.dgn@outlook.com>
2023-09-07 00:15:08 +0300

    adding login FE page

--- routers/auth_R.js
+++ routers/auth_R.js
@@ -3,6 +3,14 @@ const router = express.Router();
 module.exports = router;
 
 const auth_mid=require("../middleware/auth_mid");
+router.get("/login",function (req,res,next){
+        res.render("login",{});
+});
 router.post("/login",[auth_mid.check_login],function (req,res,next){
-        return res.send(res.loggedEn);
+        // return res.send(res.loggedEn);
+        if(res.loggedEn) {
+                return res.redirect("/p/1");
+        } else {
+                return res.redirect("/login");
+        }
 });
